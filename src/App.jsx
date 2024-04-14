8d99f2a4d352fab60d5d3989dd901313832c1a46
austen <austen.edge@xtra.co.nz>
2024-04-15 11:01:24 +1200

    user storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import List from "./components/list/List"
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
-
+import { useEffect } from "react";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <List />
-      <Chat />
-      <Detail />
-    </div>
-  )
-}
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
 
-export default App
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
+  return (
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
+    </div>
+  );
+};
+
+export default App;
