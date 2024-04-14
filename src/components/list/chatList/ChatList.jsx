bc0430ca7616a093aee5457d4db4667fbc943f9b
austen <austen.edge@xtra.co.nz>
2024-04-14 15:15:31 +1200

    feat emojis

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,8 @@
-import "./chatList.css"
+import { useState } from "react";
+import "./chatList.css";
 
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false);
     return (
         <div className='chatList'>
             <div className="search">
@@ -8,7 +10,40 @@ const ChatList = () => {
                     <img src="./search.png" alt="" />
                     <input type="text" placeholder="Search" />
                 </div>
-                <img src="./plus.png" alt="" className="add"/>
+                <img 
+                src={addMode ? "./minus.png" : "./plus.png"} 
+                alt=""
+                className="add" 
+                onClick={() => setAddMode((prev) => !prev)}
+                 />
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
             </div>
         </div>
     )
