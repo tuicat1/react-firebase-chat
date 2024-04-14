e1cf1ced31e679daa4052fd8b4806899b9de53be
austen <austen.edge@xtra.co.nz>
2024-04-14 12:43:03 +1200

    filling components

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,17 @@
+import "./chatList.css"
+
+const ChatList = () => {
+    return (
+        <div className='chatList'>
+            <div className="search">
+                <div className="searchBar">
+                    <img src="./search.png" alt="" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <img src="./plus.png" alt="" className="add"/>
+            </div>
+        </div>
+    )
+}
+
+export default ChatList
